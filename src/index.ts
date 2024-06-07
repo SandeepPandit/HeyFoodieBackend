7835b780580ab9c3977002796d4e5818175c38bc
Sandeep Pandit <sandeep.pandit.messi.10@gmail.com>
2024-06-07 14:23:20 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
-
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then(() => console.log("Connected to database!"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -19,7 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
-
+app.use("/api/my/restaurant", myRestaurantRoute);
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`server started on PORT: ${PORT}`);
