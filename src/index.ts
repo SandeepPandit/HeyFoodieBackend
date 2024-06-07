b02afad881749e0ef1848d0b77afb3d66b9614a5
Sandeep Pandit <sandeep.pandit.messi.10@gmail.com>
2024-06-08 00:07:58 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
 mongoose
   .connect(process.env.MONGODB_URI as string)
@@ -27,6 +28,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`server started on PORT: ${PORT}`);
